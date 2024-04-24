d19a371cd52760bdfb839013fc18af6a7c049400
Kaushik Rathva <m4gkavi@gmail.com>
2024-04-24 10:04:25 +0530

    added client server code for FL

---
+++ Fedrated Learning/FL-server.py
@@ -0,0 +1,36 @@
+import socket
+import threading
+
+clients_list = []
+
+# Function to start the server
+def start_server():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = socket.gethostname()
+    port = 64852
+    server_socket.bind((host, port))
+    server_socket.listen(10)  # Listen for up to 10 connections
+    server_connection_thread = threading.Thread(target=server_connection_loop,args=server_socket)
+    server_connection_thread.start()
+
+def server_connection_loop(server_socket):
+    while True:
+        client_socket, addr = server_socket.accept()
+        print(f'Connected to {addr}')
+        clients_list.append(client_socket,addr)
+        # client_thread = threading.Thread(target=handle_client, args=(client_socket, addr))
+        # client_thread.start()
+
+# Function to send a message to a client
+def send_message(client_socket, message):
+    client_socket.send(message.encode())
+
+# Function to receive a message from a client
+def receive_message(client_socket):
+    data = client_socket.recv(1024)
+    return data.decode()
+
+if __name__ == "__main__":
+    socket = start_server()
+
+    
